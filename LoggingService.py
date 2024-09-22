75cc8268b31058a2627ee99a17abcc434345c441
rudabada <71153954+rudabada@users.noreply.github.com>
2024-09-22 13:03:19 -0400

    debugging SSL handshake issue

--- LoggingService.py
+++ LoggingService.py
@@ -12,7 +12,7 @@ ca = certifi.where()
 class LoggingService:
     def __init__(self,uri,max_retries = 3,delay=5):
         # Load DB
-        client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=ca)
+        client = MongoClient(uri, tlsCAFile=ca)
         for attempt in range(max_retries):
             try:
                 client.admin.command('ping')
