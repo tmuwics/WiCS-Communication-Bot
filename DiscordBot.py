e6764703323cbc9006cafade290436f1975da813
tmuwics <wics@torontomu.ca>
2024-09-24 17:27:08 -0400

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -187,7 +187,7 @@ async def send_offseason_roles():
                 except Exception as ex:
                     logger.set_failed_shared_status(hash_id)
                     logger.log_post_failure(data,f"Offseason role {company} failed to send",str(ex))
-                time.sleep(20)
+                time.sleep(5)
 
     except(AttributeError) as err:
         logger.log_task_exception("TASK ERROR: While sending Offseason roles",err)
